0ded46e42b65ba6e1a1a710d369e711c853fa15f
Harsh Shah <harshnshah123@gmail.com>
2024-11-10 12:27:12 +0530

    Used MongoDB Atlas 2

--- server/index.js
+++ server/index.js
@@ -7,7 +7,15 @@ import restaurantRoutes from './routes/restaurantRoutes.js';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+const allowedOrigins = ['https://sefooddelivery.netlify.app/'];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true
+}));
+
+// app.use(cors());
 app.use(express.json({limit: '50mb'}));
 // app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
