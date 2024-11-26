7149d180df127de90401271f6e1441d742045fb1
aayushmohite23 <aayushmohite23@gmail.com>
2024-11-26 15:46:05 +0530

    Updated completely!

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json({limit: '50mb'}));
 // app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
-app.use('/api/restaurant', restaurantRoutes);
+app.use('https://se-food-delivery.onrender.com/api/restaurant', restaurantRoutes);
 
 const start = async () => {
     try
